3e85ce38bc4f291e30e41ec3aba859e5980aa57c
borjasalguero <fbsc@tid.es>
2015-03-04 13:22:26 +0100

    WIP: First photo taken

---
+++ js/app.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+  var _videoStream;
+
+  var videoPreviewElement;
+  var buttonElement = document.getElementById('take-photo-button');
+
+  navigator.mozGetUserMedia(
+    {
+      video: true,
+      audio: false
+    },
+    function(stream) {
+      _videoStream = stream.getVideoTracks()[0];
+      videoPreviewElement = document.getElementById('photo-preview');
+
+      videoPreviewElement.muted = true;
+      videoPreviewElement.mozSrcObject = stream;
+      videoPreviewElement.play();
+    },
+    function(error) {
+      console.log('Error when taking picture');
+    }
+  );
+
+
+  buttonElement.addEventListener(
+    'click',
+    function() {
+      var imageCapturer = new ImageCapture(_videoStream);
+      imageCapturer.onphoto = function(blobEvent, b) {
+        document.getElementById('photo-taken').src = window.URL.createObjectURL(blobEvent.data);
+        videoPreviewElement.classList.add('hidden');
+        buttonElement.classList.add('hidden');
+      };
+      imageCapturer.takePhoto();
+    }
+  );
+}
